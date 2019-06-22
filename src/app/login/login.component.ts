bdcf876245dde2b51cf0b41c93b65e8dff8458b6
nour <nour.f.zarouq@gmail.com>
2019-06-22 10:33:22 +0300

    edit input pass

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterModule, Router } from '@angular/router';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-login',
@@ -11,6 +12,16 @@ export class LoginComponent implements OnInit {
   constructor(private router: Router) { }
 
   ngOnInit() {
+    $('.toggle-password').hover(function() {
+
+      $(this).toggleClass('fa-eye fa-eye-slash');
+      const input = $($(this).attr('toggle'));
+      if (input.attr('type') === 'password') {
+        input.attr('type', 'text');
+      } else {
+        input.attr('type', 'password');
+      }
+    });
   }
 
   login() {
